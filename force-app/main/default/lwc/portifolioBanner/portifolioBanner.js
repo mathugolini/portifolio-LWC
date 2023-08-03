56fc19710a59c6b305e07c424feed60e0e2ed5db
Matheus <mathugolini@gmail.com>
2023-08-03 20:57:04 -0300

    Função portfolioHandler JS adicionada

--- force-app/main/default/lwc/portifolioBanner/portifolioBanner.js
+++ force-app/main/default/lwc/portifolioBanner/portifolioBanner.js
@@ -1,5 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire} from 'lwc';
 import PortfolioAssets from '@salesforce/resourceUrl/PortfolioAssets'
+import {getRecord} from 'lightning/uiRecordApi'
+import FULLNAME from '@salesforce/schema/Portfolio__c.FullName__c'
+import COMPANY_LOCATION from '@salesforce/schema/Portfolio__c.FullName__c'
+import COMPANY_NAME from '@salesforce/schema/Portfolio__c.CompanyLocation__c'
+import DESIGNATION from '@salesforce/schema/Portfolio__c.Designation__c'
+
 
 export default class PortfolioBanner extends LightningElement {
 
@@ -11,4 +17,15 @@ export default class PortfolioBanner extends LightningElement {
     linkedin = `${PortfolioAssets}/PortfolioAssets/Social/linkedin.svg`
     github = `${PortfolioAssets}/PortfolioAssets/Social/github.svg`
     trailhead = `${PortfolioAssets}/PortfolioAssets/Social/trailhead1.svg`
+
+    recordId = 'a01Hu00000x9SiYIAU'
+    @wire(getRecord, {recordId:this.recordId, fields:[FULLNAME, COMPANY_LOCATION, COMPANY_NAME, DESIGNATION]})
+    portfolioHandler({data, error}) {
+        if(data) {
+            console.log("record Data", JSON.stringify(data))
+        } 
+        if (error) {
+            console.error("error", error)
+        }
+    }
 }

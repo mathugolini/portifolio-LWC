64ff38a38559d4f3c7cae3381bf98ada731e2522
Matheus <mathugolini@gmail.com>
2023-08-03 20:42:17 -0300

    Implementando static resources na classe JS e estilizando componentes css/html

--- force-app/main/default/lwc/portifolioBanner/portifolioBanner.js
+++ force-app/main/default/lwc/portifolioBanner/portifolioBanner.js
@@ -3,5 +3,12 @@ import PortfolioAssets from '@salesforce/resourceUrl/PortfolioAssets'
 
 export default class PortfolioBanner extends LightningElement {
 
+    linkedinUrl = "https://www.linkedin.com/in/matheus-hugolini/"
+    githubUrl = "https://github.com/mathugolini"
+    trailheadUrl = "https://www.salesforce.com/trailblazer/mathugolini"
+
     userPic = `${PortfolioAssets}/PortfolioAssets/matheusLogo.png`
+    linkedin = `${PortfolioAssets}/PortfolioAssets/Social/linkedin.svg`
+    github = `${PortfolioAssets}/PortfolioAssets/Social/github.svg`
+    trailhead = `${PortfolioAssets}/PortfolioAssets/Social/trailhead1.svg`
 }

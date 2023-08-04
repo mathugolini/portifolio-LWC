a093d36ceef5d6cd56a452c324b9083e92283d79
Matheus <mathugolini@gmail.com>
2023-08-03 21:15:38 -0300

    Implementando metodos get com base no model

--- force-app/main/default/lwc/portifolioBanner/portifolioBanner.js
+++ force-app/main/default/lwc/portifolioBanner/portifolioBanner.js
@@ -1,9 +1,9 @@
 import { LightningElement, wire} from 'lwc';
 import PortfolioAssets from '@salesforce/resourceUrl/PortfolioAssets'
-import {getRecord} from 'lightning/uiRecordApi'
+import {getRecord, getFieldValue} from 'lightning/uiRecordApi'
 import FULLNAME from '@salesforce/schema/Portfolio__c.FullName__c'
-import COMPANY_LOCATION from '@salesforce/schema/Portfolio__c.FullName__c'
-import COMPANY_NAME from '@salesforce/schema/Portfolio__c.CompanyLocation__c'
+import COMPANY_LOCATION from '@salesforce/schema/Portfolio__c.CompanyLocation__c'
+import COMPANY_NAME from '@salesforce/schema/Portfolio__c.CompanyName__c'
 import DESIGNATION from '@salesforce/schema/Portfolio__c.Designation__c'
 
 
@@ -20,12 +20,32 @@ export default class PortfolioBanner extends LightningElement {
 
     recordId = 'a01Hu00000x9SiYIAU'
     @wire(getRecord, {recordId:'$recordId', fields:[FULLNAME, COMPANY_LOCATION, COMPANY_NAME, DESIGNATION]})
-    portfolioHandler({data, error}) {
-        if(data) {
-            console.log("record Data", JSON.stringify(data))
-        } 
-        if (error) {
-            console.error("error", error)
-        }
+    portfolioData
+
+    // verificar qual dado esta retornando no console --- usar inspect
+// portfolioHandler({data, error}) {
+//     if(data) {
+//        console.log("record Data", JSON.stringify(data))
+//     } 
+//      if (error) {
+//          console.error("error", error)
+//       }
+//   }
+
+    get fullName() {
+        return getFieldValue(this.portfolioData.data, FULLNAME)
     }
+
+    get companyLocation() {
+        return getFieldValue(this.portfolioData.data, COMPANY_LOCATION)
+    }
+
+    get companyName() {
+        return getFieldValue(this.portfolioData.data, COMPANY_NAME)
+    }
+
+    get designation() {
+        return getFieldValue(this.portfolioData.data, DESIGNATION)
+    }
+
 }

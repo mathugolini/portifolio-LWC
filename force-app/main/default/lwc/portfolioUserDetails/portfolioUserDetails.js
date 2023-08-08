84386e9e027987e838f59103882c3ff373d91e0c
Matheus <mathugolini@gmail.com>
2023-08-08 16:32:40 -0300

    Adicionando função de download do resume

--- force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
+++ force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
@@ -6,7 +6,7 @@ export default class PortfolioUserDetails extends LightningElement {
     @api objectApiName
 
     downloadResume() {
-
+        window.open("https://github.com/mathugolini/Matheus-Resume/raw/main/Matheus%20Hugolini%20-%20Resume%20(Updated).pdf","_blank")
     }
 
 }
